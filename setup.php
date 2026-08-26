fc9e996642f7ea176c446ec0c6755300d541acfa
Marco Masson <marco.masson@educacao.sp.gov.br>
2026-08-26 10:21:01 -0300

    feat: registra rotas stateless no SessionManager do GLPI 11 para bypass de CSRF e Firewall

--- setup.php
+++ setup.php
@@ -5,6 +5,19 @@ use GlpiPlugin\Whatsappsimples\Menu;
 
 include_once __DIR__ . '/hook.php';
 
+// Registra os caminhos STATELESS (API / Webhook sem sessão e sem CSRF) no GLPI 11
+if (class_exists(\Glpi\Http\SessionManager::class)) {
+    \Glpi\Http\SessionManager::registerPluginStatelessPath('whatsappsimples', '#^/webhook#');
+    \Glpi\Http\SessionManager::registerPluginStatelessPath('whatsappsimples', '#^/front/webhook.php#');
+    \Glpi\Http\SessionManager::registerPluginStatelessPath('whatsappsimples', '#^/public/webhook.php#');
+}
+
+if (class_exists(\Glpi\Http\Firewall::class)) {
+    \Glpi\Http\Firewall::addPluginStrategyForLegacyScripts('whatsappsimples', '#^/front/webhook.php#', \Glpi\Http\Firewall::STRATEGY_NO_CHECK);
+    \Glpi\Http\Firewall::addPluginStrategyForLegacyScripts('whatsappsimples', '#^/public/webhook.php#', \Glpi\Http\Firewall::STRATEGY_NO_CHECK);
+    \Glpi\Http\Firewall::addPluginStrategyForLegacyScripts('whatsappsimples', '#^/webhook#', \Glpi\Http\Firewall::STRATEGY_NO_CHECK);
+}
+
 function plugin_init_whatsappsimples(): void
 {
     global $PLUGIN_HOOKS;
@@ -12,10 +25,16 @@ function plugin_init_whatsappsimples(): void
     $PLUGIN_HOOKS['csrf_compliant']['whatsappsimples'] = true;
     $PLUGIN_HOOKS['change_profile']['whatsappsimples'] = ['PluginWhatsappsimplesProfile', 'changeProfile'];
 
+    if (class_exists(\Glpi\Http\SessionManager::class)) {
+        \Glpi\Http\SessionManager::registerPluginStatelessPath('whatsappsimples', '#^/webhook#');
+        \Glpi\Http\SessionManager::registerPluginStatelessPath('whatsappsimples', '#^/front/webhook.php#');
+        \Glpi\Http\SessionManager::registerPluginStatelessPath('whatsappsimples', '#^/public/webhook.php#');
+    }
+
     if (class_exists(\Glpi\Http\Firewall::class)) {
         \Glpi\Http\Firewall::addPluginStrategyForLegacyScripts('whatsappsimples', '#^/front/webhook.php#', \Glpi\Http\Firewall::STRATEGY_NO_CHECK);
         \Glpi\Http\Firewall::addPluginStrategyForLegacyScripts('whatsappsimples', '#^/public/webhook.php#', \Glpi\Http\Firewall::STRATEGY_NO_CHECK);
-        \Glpi\Http\Firewall::addPluginStrategyForLegacyScripts('whatsappsimples', '#^/webhook.php#', \Glpi\Http\Firewall::STRATEGY_NO_CHECK);
+        \Glpi\Http\Firewall::addPluginStrategyForLegacyScripts('whatsappsimples', '#^/webhook#', \Glpi\Http\Firewall::STRATEGY_NO_CHECK);
     }
 
     Plugin::registerClass('PluginWhatsappsimplesProfile', ['addtabon' => 'Profile']);
