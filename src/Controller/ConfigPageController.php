993c05c31af7368799e8e934f22caebd49257606
Marco Masson <marco.masson@educacao.sp.gov.br>
2026-08-27 13:24:36 -0300

    fix: adiciona .php na chamada ajax da pagina de configuracao e remove AbstractController do GetQrCodeController para evitar erro 500

--- src/Controller/ConfigPageController.php
+++ src/Controller/ConfigPageController.php
@@ -136,7 +136,7 @@ final class ConfigPageController extends AbstractController
 
                 try {
                     const csrfToken = (typeof CFG_GLPI !== 'undefined' && CFG_GLPI.csrf_token) ? CFG_GLPI.csrf_token : '';
-                    const res = await fetch(`${rootDoc}/plugins/whatsappsimples/ajax/qrcode`, {
+                    const res = await fetch(`${rootDoc}/plugins/whatsappsimples/ajax/qrcode.php`, {
                         headers: {
                             'X-Requested-With': 'XMLHttpRequest',
                             'X-Glpi-Csrf-Token': csrfToken
